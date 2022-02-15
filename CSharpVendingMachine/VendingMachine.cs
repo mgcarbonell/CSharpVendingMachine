3573badf0b504d1f786381345c26f931e5b3f266
mgcarbonell <mgcarbonell@gmail.com>
2022-02-15 13:57:53 -0800

    creats vending machine class

---
+++ CSharpVendingMachine/VendingMachine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpVendingMachine
+{
+    class VendingMachine
+    {
+        int count;
+        int capacity;
+        decimal moneyCollected;
+        string manufacturer;
+
+        void Display()
+        {
+
+        }
+        void DispenseProducts()
+        {
+
+        }
+
+        void Refill()
+        {
+
+        }
+    }
+}
