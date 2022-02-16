834c9295aec5c174fdb2b2dd2a8adc4d894c8070
mgcarbonell <mgcarbonell@gmail.com>
2022-02-15 17:38:53 -0800

    Works through naming conventions and methods.

--- CSharpVendingMachine/Program.cs
+++ CSharpVendingMachine/Program.cs
@@ -5,7 +5,11 @@ namespace CSharpVendingMachine
     {
         public static void Main(string[] args)
         {
+            VendingMachine vendy3000 = new VendingMachine();
 
+            //Console.WriteLine(vendy3000.GetManufacturer());
+            vendy3000.Count = 88;
+            Console.WriteLine("The count is: {0}", vendy3000.Count);
         }
     }
 }
--- CSharpVendingMachine/VendingMachine.cs
+++ CSharpVendingMachine/VendingMachine.cs
@@ -4,24 +4,68 @@ using System.Collections.Generic;
 namespace CSharpVendingMachine
 {
     class VendingMachine
+
     {
-        int count;
-        int capacity;
-        decimal moneyCollected;
-        string manufacturer;
+        public const string Manufacturer = "MGC Inc.";
+
+        private int _count = 0;
+        private decimal _moneyCollected;
+
+        private static int _capacity = 100;
+        
+        public bool Permission = true;
+
+        public int Count 
+        { 
+            get
+            {
+                return _count;
+            }
+
+            set
+            {
+                if (value >= 0 && value <= _capacity)
+                {
+                    _count = value;
+                }
+            }
+        
+        }
 
         void Display()
         {
-
+            Console.WriteLine("This.Display  - hah-hah, class joke - this is the Display method.");
         }
         void DispenseProducts()
         {
-
+            Console.WriteLine("You have accessed the DispenseProducts method.");
         }
 
         void Refill()
         {
+            Console.WriteLine("This is the refill method, I will refill count.");
+        }
 
+        public string GetManufacturer()
+        {
+            return Manufacturer;
+        }
+
+        public void CountMoney()
+        {
+            if(this.Permission)
+            {
+                Console.WriteLine("We have collected ${0} today", this._moneyCollected);
+            }
+            else
+            {
+                int i = 0;
+                while (i < 10)
+                {
+                    Console.WriteLine("YOU DIDN'T SAY THE MAGIC WORD!");
+                    i++;
+                }
+            }
         }
     }
 }
