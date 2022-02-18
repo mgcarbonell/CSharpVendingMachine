d181babd67df7da2d22397de06244c7cdc349e68
mgcarbonell <mgcarbonell@gmail.com>
2022-02-17 20:41:01 -0800

    Final commit for the night, works through inheritance. Hybrid and multiple inheritance are applicable using interfaces only.

--- CSharpVendingMachine/Program.cs
+++ CSharpVendingMachine/Program.cs
@@ -6,17 +6,10 @@ namespace CSharpVendingMachine
         public static void Main(string[] args)
         {
             VendingMachine vendy = new VendingMachine(true, 50, 10);
-            //var vendy2 = new VendingMachine(true, 50, 10);
-            //var badVendy = new VendingMachine(true, 110, -10);
-
-            //Console.WriteLine(vendy3000.GetManufacturer());
-            //vendy.PrintFields();
-            //vendy2.PrintFields();
-            //badVendy.PrintFields();
-            //Beverage cola = new Beverage("RC Cola", 0.7, "8/12/2027", 0.35, "Cola");
-            //cola.BeverageDetails();
-            var cola = new Soda("CocaCola", 0.9, "12/12/2025", 0.35, "Cola", false, true);
+            var cola = new Soda("RC Cola", 0.9, "12/12/2027", 350, "Cola", true, false);
+            var crunch = new Chocolate("Crunch", 2.3, "12/12/2027", 43, true);
             cola.SodaDetails();
+            crunch.ChocolateDetails();
         }
     }
 }
